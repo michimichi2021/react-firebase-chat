b32571e36aa109384696591cd493d7d57e094677
michimichi2021 <michikaho1009@gmail.com>
2022-08-16 00:22:51 +0900

    状態をリセットした

--- README.md
+++ README.md
@@ -1,13 +1,1 @@
-# セットアップ方法
-
-npx create-react-app my-app --templete typescript
-
-実行出来たら、以下のコマンドを順番に実行します。
-
-cd my-app
-npm start
-
-ブラウザで画面を確認する際は、
-[http://localhost:3000](http://localhost:3000)
-で確認する。
 
--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,5 @@
-import { createTheme, ThemeProvider } from "@mui/material/styles";
-import { BrowserRouter, Routes, Route, Outlet } from "react-router-dom";
-import Signup from "./Signup";
-import Login from "./Login";
-import Home from "./Home";
-import Header from "./components/Header";
-import PasswordReset from "./PasswordReset";
-
-
-const theme = createTheme()
-
-function Layout() {
-  return (
-    <>
-      <Header />
-      <Outlet />
-    </>
-  )
-}
-
 export default function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <BrowserRouter>
-        <Routes>
-          <Route element={<Layout />}>
-            <Route path="/" element={<Home />} />
-          </Route>
-          <Route path="login" element={<Login />} />
-          <Route path="signup" element={<Signup />} />
-          <Route path="password-reset" element={<PasswordReset />} />
-        </Routes>
-      </BrowserRouter>
-    </ThemeProvider>
   );
 }
 
