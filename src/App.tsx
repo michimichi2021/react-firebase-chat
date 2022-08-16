2b1a90ad921e1e85152f88a9de16817e19409d98
michimichi2021 <michikaho1009@gmail.com>
2022-08-16 15:22:36 +0900

    App.tsxにMUIのHome.tsxとLogin.tsxをインポート

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
-import { createTheme, ThemeProvider } from "@mui/material/styles"
-
-import Signup from "./Signup"
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Home";
+import Signup from "./Signup";
+import Login from "./Login";
 
 const theme = createTheme()
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <Signup />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="login" element={<Login />} />
+          <Route path="signup" element={<Signup />} />
+        </Routes>
+      </BrowserRouter>
     </ThemeProvider>
   )
 }
